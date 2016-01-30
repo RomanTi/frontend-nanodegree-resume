2fd93f1e1a95f4c9e7460d926a5250fd3f2ea1a9
Roman Tiulmankov <rtiulmankov@oandacorp.com>
2016-01-30 13:20:22 -0500

    step2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,34 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var name = "Roman Tiulmankov";
+var role = "Web Developer";
+
+var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+
+$("#header").prepend(formattedRole)
+$("#header").prepend(formattedName)
+
+
+var name = "AlbERt EINstEiN";
+
+function nameChanger(oldName) {
+    var finalName = oldName;
+    // Your code goes here!
+
+    var spaceIndex = finalName.indexOf(" ");
+    var firstName = finalName.substring(0,1).toUpperCase()+finalName.substring(1,spaceIndex).toLowerCase();
+    var lastName = finalName.substring(spaceIndex).toUpperCase();
+    finalName = firstName.concat(lastName);
+     
+    
+    // Don't delete this line!
+    return finalName;
+};
+
+// Did your code work? The line below will tell you!
+console.log(nameChanger(name));
+
+
+
